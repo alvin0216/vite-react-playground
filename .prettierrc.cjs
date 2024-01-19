3d5125c61bf8277880c0d1251c34f942bc49f276
alvin <alvin00216@163.com>
2024-01-20 01:58:10 +0800

    x

--- .prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-
-module.exports = {
-    tabWidth: 2,
-    plugins: ["prettier-plugin-tailwindcss"],
-    tailwindConfig: "./tailwind.config.js",
-};
--- src/demos/Demo1.tsx
+++ src/demos/Demo1.tsx
@@ -4,16 +4,16 @@ import Mock from "mockjs";
 const data = Mock.mock("@paragraph");
 
 const Demo1: React.FC = () => {
-    return (
-        <>
-            <Button variant="contained">Click</Button>
-            <div className="text-cyan-500">{data}</div>
+  return (
+    <>
+      <Button variant="contained">Click</Button>
+      <div className="text-cyan-500">{data}</div>
 
-            <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-                ...
-            </button>
-        </>
-    );
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </>
+  );
 };
 
 export default Demo1;
