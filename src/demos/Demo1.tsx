929be7f1888b565b672cd624169c2ed25e57d5f9
alvin <alvin00216@163.com>
2024-01-20 01:04:48 +0800

    add demo

--- src/demos/Demo1.tsx
+++ src/demos/Demo1.tsx
@@ -1,29 +1,52 @@
-import { Button } from '@mui/material';
-import { useRequest } from 'ahooks';
-import Mock from 'mockjs';
+// Import Swiper React components
+import { Swiper, SwiperSlide, SwiperClass } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Button, Grid } from '@mui/material';
+import { Navigation } from 'swiper/modules';
+import { useRef } from 'react';
 
 const Demo1: React.FC = () => {
-  const { data, params, loading, run } = useRequest<{ text: string; time: number }>(() => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          text: Mock.mock('@paragraph'),
-          time: new Date().getTime(),
-        });
-      }, 3000);
-    });
-  });
+  const swipperRef = useRef<SwiperClass | null>(null);
+  const list = [
+    [1, 2, 3, 4],
+    [4, 5, 6, 7],
+    [8, 9],
+  ];
 
-  console.log('%c params:', 'color: red', params);
   return (
-    <>
-      <Button variant='contained' onClick={run}>
-        Click
-      </Button>
-      <div className='text-[red]'> {loading && 'loading'}</div>
-      <p>{data?.time}</p>
-      <p>{data?.text}</p>
-    </>
+    <Swiper
+      navigation
+      slidesPerView={1}
+      modules={[Navigation]}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => {
+        swipperRef.current = swiper;
+      }}>
+      {list.map((items, iIndex) => (
+        <SwiperSlide key={iIndex}>
+          <Grid container spacing={3} alignItems='stretch'>
+            {items.map((item, itemIndecx) => (
+              <Grid key={itemIndecx} item xs={3}>
+                <div className='bg-purple-100 w-full'>
+                  <Button
+                    variant='contained'
+                    onFocus={() => {
+                      if (swipperRef.current?.activeIndex !== iIndex) {
+                        swipperRef.current?.slideTo(iIndex);
+                      }
+                    }}>
+                    {item}
+                  </Button>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
