d5635a9dce12221c7c3958081f9bde68667b3cc9
Alvin <guosw5@lenovo.com>
2024-07-05 09:21:50 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import Demo1 from './demos/Demo1';
+import { atom, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useEventTarget } from 'ahooks';
+
+const todoState = atom({ key: 'to', default: ['default'] as string[] });
+
+const TextInput = () => {
+  const [value, { onChange }] = useEventTarget({ initialValue: '' });
+  const setTodoList = useSetRecoilState(todoState);
+
+  const add = () => {
+    if (value) setTodoList((prev) => [...prev, value]);
+  };
+  return (
+    <>
+      <input type='text' value={value} onChange={onChange} />
+      <button onClick={add}>add</button>
+    </>
+  );
+};
+
+const TodoList = () => {
+  const todoList = useRecoilValue(todoState);
+  return (
+    <ul>
+      {todoList.map((t, i) => (
+        <li key={i}>{t}</li>
+      ))}
+    </ul>
+  );
+};
+
+const Sibling = () => {
+  console.log('render Sibling');
+  return null;
+};
 
 const App: React.FC = () => {
+  console.log('render App');
   return (
-    <div className='container mx-auto mt-10'>
-      <Demo1 />
-    </div>
+    <>
+      <TextInput />
+      <TodoList />
+      <Sibling />
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import App from './App.tsx';
 import 'virtual:uno.css';
 import '@unocss/reset/sanitize/sanitize.css';
 import '@unocss/reset/sanitize/assets.css';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
